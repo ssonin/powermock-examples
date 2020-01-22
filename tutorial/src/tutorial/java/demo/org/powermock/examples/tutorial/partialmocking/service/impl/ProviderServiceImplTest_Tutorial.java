5d5ee1179f47c8200910be7a1de152c98293acc7
Sergey <sergey_sonin@epam.com>
2020-01-22 13:22:13 +0300

    Add Mockito 2 solution for the first drill of partialmocking

--- tutorial/src/tutorial/java/demo/org/powermock/examples/tutorial/partialmocking/service/impl/ProviderServiceImplTest_Tutorial.java
+++ tutorial/src/tutorial/java/demo/org/powermock/examples/tutorial/partialmocking/service/impl/ProviderServiceImplTest_Tutorial.java
@@ -15,10 +15,26 @@
  */
 package demo.org.powermock.examples.tutorial.partialmocking.service.impl;
 
-import demo.org.powermock.examples.tutorial.partialmocking.dao.ProviderDao;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
+import static org.powermock.reflect.Whitebox.setInternalState;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import demo.org.powermock.examples.tutorial.partialmocking.dao.ProviderDao;
+import demo.org.powermock.examples.tutorial.partialmocking.domain.ServiceProducer;
 
 /**
  * The purpose of this test is to get 100% coverage of the
@@ -29,8 +45,8 @@ import org.junit.Test;
  * While doing this tutorial please refer to the documentation on how to expect
  * private methods and bypass encapsulation at the PowerMock web site.
  */
-// TODO Specify the PowerMock runner
-// TODO Specify which classes that must be prepared for test
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ProviderServiceImpl.class)
 public class ProviderServiceImplTest_Tutorial {
 	
 	private ProviderServiceImpl tested;
@@ -39,24 +55,31 @@ public class ProviderServiceImplTest_Tutorial {
 
 	@Before
 	public void setUp() {
-		// TODO Create a mock object of the ProviderDao class
-		// TODO Create a new instance of ProviderServiceImpl
-		// TODO Set the providerDao mock to the providerDao field in the tested instance
+		this.providerDaoMock = mock(ProviderDao.class);
+		this.tested = new ProviderServiceImpl();
+		setInternalState(this.tested, this.providerDaoMock);
 	}
 
 	@After
 	public void tearDown() {
-		// TODO Set all references to null
+		this.providerDaoMock = null;
+		this.tested = null;
 	}
 
 	@Test
 	public void testGetAllServiceProviders() throws Exception {
-		// TODO Create a partial mock of the ProviderServiceImpl mocking only the getAllServiceProducers method
-		// TODO Create a new HashSet of ServiceProducer's and add a ServiceProducer to the set
-		// TODO Expect the private method call to getAllServiceProducers and return the created HashSet
-		// TODO Replay all mock objects used
-		// TODO Perform the actual test and assert that the result matches the expectations  
-		// TODO Verify all mock objects used
+		// given
+		Set<ServiceProducer> expectedProducers = new HashSet<>();
+		expectedProducers.add(new ServiceProducer(42, "some name"));
+		this.tested = spy(new ProviderServiceImpl());
+		doReturn(expectedProducers).when(this.tested, "getAllServiceProducers");
+
+		// when
+		Set<ServiceProducer> actualProducers = this.tested.getAllServiceProviders();
+
+		// then
+		verifyPrivate(this.tested).invoke("getAllServiceProducers");
+		assertThat(actualProducers, sameInstance(expectedProducers));
 	}
 
 	@Test
